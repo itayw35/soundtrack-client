e015d1969d3d29b61564e288931014c351ae7838
itay weinberg <weinbergitay@gmail.com>
2023-05-11 10:56:48 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Routes,Route, Navigate} from "react-router-dom"
+import Main from './components/Main';
+import Map from "./components/Map";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Routes>
+    <Route path='/' element={<Navigate to="/home"/>}/>
+<Route path="/home" element={ <Main/>}/>
+<Route path="/tracks/:trackId" element={ <Map/>}/>
+    </Routes>
+   
     </div>
-  );
+  )
 }
-
-export default App;
