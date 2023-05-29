f627939c72026467657dc86783fd1e35bd27e40a
itay weinberg <weinbergitay@gmail.com>
2023-05-29 18:08:31 +0300

    solving legging in usertracks

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,7 +29,7 @@ export default function Main() {
         })
         .catch((err) => console.log(err));
     }
-  }, []);
+  }, [isLoggedIn]);
 
   return (
     <>
--- src/components/Track.js
+++ src/components/Track.js
@@ -41,7 +41,7 @@ function Track(props) {
   };
   useEffect(() => {
     setIsTempAvailable(false);
-  }, [props.isLoggedIn]);
+  }, [props.userTracks]);
   return props.track.isAvailable &&
     (props.userTracks.includes(props.track._id) || isTempAvailable) ? (
     <Link className="track-link" to={`/tracks/${props.track.trackName}`}>
