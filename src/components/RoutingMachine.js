77bc348ad728bce0002c4e00c4dc0d91e6e74281
itay weinberg <weinbergitay@gmail.com>
2023-06-01 23:32:11 +0300

    test

--- src/components/RoutingMachine.js
+++ src/components/RoutingMachine.js
@@ -1,11 +1,8 @@
 import L from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine"
-import { useEffect } from "react";
 const createRoutineMachineLayer = (props)=>{
-    useEffect(()=>{
-        console.log(process.env.MAPBOX_API_KEY);
-    },[])
+   
     const markers= props.markers.map((marker)=>{return L.latLng(marker.latitude,marker.longitude)})
     const instance = L.Routing.control({
         waypoints:[
