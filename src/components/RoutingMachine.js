8c6dbcb35f96fd5aa236e5cb8b399d44037c7ab1
itay weinberg <weinbergitay@gmail.com>
2023-06-08 19:20:12 +0300

    test

--- src/components/RoutingMachine.js
+++ src/components/RoutingMachine.js
@@ -36,18 +36,18 @@ const createRoutineMachineLayer = (props) => {
     console.log(summary);
     console.log(instructions);
   });
-  fetch(
-    "https://api.mapbox.com/directions/v5/mapbox/walking/34.74957,32.05399;34.74957,32.05399;34.75316,32.05439;34.75204,32.05475;34.75284,32.05547;34.75627,32.05505;34.75627,32.05505?overview=false&alternatives=true&steps=true&access_token=sk.eyJ1IjoiaXRheXciLCJhIjoiY2xpZGwwNTk0MDFxYzNkcGR1OG5ycDljNCJ9.qIz_ana-9ikzkETbPwsBQA"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Process the response data
-      console.log(data);
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error(error);
-    });
+  // fetch(
+  //   `https://api.mapbox.com/directions/v5/mapbox/walking/34.74957,32.05399;34.74957,32.05399;34.75316,32.05439;34.75204,32.05475;34.75284,32.05547;34.75627,32.05505;34.75627,32.05505?overview=false&alternatives=true&steps=true&access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     // Process the response data
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     // Handle any errors
+  //     console.error(error);
+  //   });
   return instance;
 };
 
