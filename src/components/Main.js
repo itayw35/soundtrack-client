6d75d723c2fd425d118844ee758a28ad70a0137f
Itay Weinberg <itayweinberg@h-iMac-sl-Itay.local>
2024-03-16 16:05:58 +0200

    adding loading notice

--- src/components/Main.js
+++ src/components/Main.js
@@ -88,7 +88,7 @@ export default function Main() {
                     />
                   );
                 })
-              : null}
+              :"Loading..."}
           </div>
         </div>
       </div>
